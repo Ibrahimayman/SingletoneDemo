a072f91e743c907a91cfa381a9902db58ae8f7c0
Ibrahim Ayman Abd Elwahab Alaswar <eng.ibrahim.cer@hotmail.com>
2021-11-12 15:19:02 +0200

    first commit

---
+++ SingletoneDemo/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletoneDemo
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
